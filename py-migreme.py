1637081e03cc24ca1246350632c0452ac15c7ec4
Guest <guest-OjboCp@lec01.(none)>
2013-06-29 15:36:09 -0300

    Exceptions (script modified after a looong time)

--- py-migreme.py
+++ py-migreme.py
@@ -1,33 +1,39 @@
 #! /usr/bin/env python
 #
 # Simple script using the migre.me API
-# Author: Luis Henrique B. Sousa <contato@luishenrique.org>
+# Author: Luis Henrique Borges <contato@luishenrique.org>
 # Date: 2009-10-16
 #
-# Usage: python migreme.py <url>
-# Example: ./migreme.py http://twitter.com/lshenr
-#	
+# Usage: ./py-migreme.py <url>
 
 from sys import argv
-import urllib
-import xml.dom.minidom
+from urllib2 import urlopen, URLError
+from xml.dom.minidom import parse
+
 
 def migreme(url):
-	api = 'http://migre.me/api.xml?url=%s'
-	mm_url = api % url
-	dom = xml.dom.minidom.parse(urllib.urlopen(mm_url))
+	mm_url = 'http://migre.me/api.xml?url=%s' % url
 
-	return str(dom.getElementsByTagName('migre')[0].childNodes[0].data)
+	try:
+		dom = parse(urlopen(mm_url))
+		if dom:
+			return str(dom.getElementsByTagName('migre')[0].childNodes[0].data)
+
+	except URLError, e:
+		print 'Error: %s' % e.reason
+	
+	return None
+
 
 def main(*args):
 	list = args[0]
 	if len(list) == 1:
-		print '\n-> Shortened URL:', migreme(str(list[0]))
+		shortened = migreme(str(list[0]))
+		if shortened:
+			print 'Shortened URL:', shortened
 	else:
-		print """
-- It works only with a URL.
-- Usage: ./%s <url>
-""" % argv[0]
+		print "Usage: ./%s <url>" % argv[0]
+
 	
 if __name__ == "__main__":
     main(argv[1:])
