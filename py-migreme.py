95cd7d954b33011844acfffc372efd37f9c61d8b
Luis Henrique <luishsamonte@gmail.com>
2013-07-09 12:36:34 -0300

    pep8

--- py-migreme.py
+++ py-migreme.py
@@ -12,24 +12,25 @@ from xml.dom.minidom import parse
 
 
 def migreme(url):
-	mm_url = 'http://migre.me/api.xml?url=%s' % url
-	dom = parse(urlopen(mm_url))
-	return str(dom.getElementsByTagName('migre')[0].childNodes[0].data)
+    mm_url = 'http://migre.me/api.xml?url=%s' % url
+    dom = parse(urlopen(mm_url))
+    return str(dom.getElementsByTagName('migre')[0].childNodes[0].data)
 
 
 def main(*args):
-	list = args[0]
-	if len(list) == 1:
-		try:
-			shortened = migreme(str(list[0]))
-			print 'Shortened URL:', shortened
-		except URLError, e:
-			print 'URLError: %s' % e.reason
-		except:
-			print 'Something bad happened'
-	else:
-		print "Usage: ./%s <url>" % argv[0]
+    list = args[0]
+    if len(list) == 1:
+        try:
+            shortened = migreme(str(list[0]))
+            print 'Shortened URL:', shortened
+        except URLError, e:
+            print 'URLError: %s' % e.reason
+        except:
+            print 'Something bad happened'
+    else:
+        print "Usage: ./%s <url>" % argv[0]
 
-	
+
 if __name__ == "__main__":
     main(argv[1:])
+
